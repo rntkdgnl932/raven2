9620b10fc4d236dfe961c1ca5f92c424d56c4ab8
coob <koosanghwi@gmail.com>
2024-06-15 15:39:44 +0900

    ver 1.173

--- data_raven2/mymodule/action_raven2.py
+++ data_raven2/mymodule/action_raven2.py
@@ -757,7 +757,7 @@ def go_random(cla):
             full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\action\\maul\\random_move.PNG"
             img_array = np.fromfile(full_path, np.uint8)
             img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-            imgs_ = imgs_set_(360, 940, 470, 1010, cla, img, 0.8)
+            imgs_ = imgs_set_(360, 940, 470, 1010, cla, img, 0.7)
             if imgs_ is not None and imgs_ != False:
                 print("random_move", imgs_)
 
