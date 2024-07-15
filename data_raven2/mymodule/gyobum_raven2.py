d2d29602c7371ebf2fc4b88115b1b13246efa7bb
coob <koosanghwi@gmail.com>
2024-07-15 20:30:57 +0900

    ver 1.2701

--- data_raven2/mymodule/gyobum_raven2.py
+++ data_raven2/mymodule/gyobum_raven2.py
@@ -291,7 +291,7 @@ def gyobum_get(cla):
                                     full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\gyobum_point_2.PNG"
                                     img_array = np.fromfile(full_path, np.uint8)
                                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                    imgs_ = imgs_set_for(50, 130, 210, 700, cla, img, 0.8)
+                                    imgs_ = imgs_set_for(50, 130, 210, 700, cla, img, 0.7)
                                     if imgs_ is not None and imgs_ != False:
                                         # print("gyobum_point_2", imgs_)
                                         last = len(imgs_)
@@ -342,7 +342,7 @@ def gyobum_get(cla):
                                                 full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\gyobum_point_2.PNG"
                                                 img_array = np.fromfile(full_path, np.uint8)
                                                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                                imgs_ = imgs_set_(50, 130, 210, 700, cla, img, 0.8)
+                                                imgs_ = imgs_set_(50, 130, 210, 700, cla, img, 0.7)
                                                 if imgs_ is not None and imgs_ != False:
 
                                                     for c in range(3):
--- data_raven2/mymodule/test_.py
+++ data_raven2/mymodule/test_.py
@@ -55,19 +55,12 @@ def go_test():
     elif cla == "six":
         plus = 960 * 5
 
-    full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\event\\get\\get_title\\5.PNG"
+    full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\gyobum_point_2.PNG"
     img_array = np.fromfile(full_path, np.uint8)
     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(200, 300, 600, 400, cla, img, 0.8)
+    imgs_ = imgs_set_(50, 130, 210, 700, cla, img, 0.7)
     if imgs_ is not None and imgs_ != False:
-        print("event...5..", imgs_)
-
-    full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\event\\allget\\allget_point_2.PNG"
-    img_array = np.fromfile(full_path, np.uint8)
-    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-    imgs_ = imgs_set_(60, 300, 225, 765, cla, img, 0.8)
-    if imgs_ is not None and imgs_ != False:
-        print("allget_point_2", imgs_)
+        print("gyobum_point_2", imgs_)
 
     # text_check_get(522, 550, 580, 580, cla)
     #
