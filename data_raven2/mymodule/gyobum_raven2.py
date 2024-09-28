b375cd3257bd4c6536e0532287991a79188e857f
coob <koosanghwi@gmail.com>
2024-09-28 23:40:19 +0900

    ver 1.51

--- data_raven2/mymodule/gyobum_raven2.py
+++ data_raven2/mymodule/gyobum_raven2.py
@@ -1,7 +1,7 @@
 import time
 # import os
 import sys
-
+from PyQt5.QtTest import *
 
 import variable as v_
 
@@ -250,14 +250,15 @@ def gyobum_get(cla):
                                                 break
                                             time.sleep(0.1)
 
-                                        full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\monster_info_close_btn.PNG"
-                                        img_array = np.fromfile(full_path, np.uint8)
-                                        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                        imgs_ = imgs_set_(500, 250, 800, 500, cla, img, 0.75)
-                                        if imgs_ is not None and imgs_ != False:
-                                            print("monster_info_close_btn", imgs_)
-                                            click_pos_reg(imgs_.x, imgs_.y, cla)
-                                            break
+                                        for c in range(3):
+                                            full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\monster_info_close_btn.PNG"
+                                            img_array = np.fromfile(full_path, np.uint8)
+                                            img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                            imgs_ = imgs_set_(500, 250, 800, 500, cla, img, 0.75)
+                                            if imgs_ is not None and imgs_ != False:
+                                                print("monster_info_close_btn", imgs_)
+                                                click_pos_reg(imgs_.x, imgs_.y, cla)
+                                            QTest.qWait(500)
 
                                 else:
 
@@ -588,15 +589,15 @@ def gyobum_get(cla):
                                                         time.sleep(0.2)
                                                         break
                                                     time.sleep(0.1)
-
-                                                full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\monster_info_close_btn.PNG"
-                                                img_array = np.fromfile(full_path, np.uint8)
-                                                img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                                                imgs_ = imgs_set_(500, 250, 800, 500, cla, img, 0.75)
-                                                if imgs_ is not None and imgs_ != False:
-                                                    print("monster_info_close_btn", imgs_)
-                                                    click_pos_reg(imgs_.x, imgs_.y, cla)
-                                                    break
+                                                for c in range(3):
+                                                    full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\gyobum\\monster_info_close_btn.PNG"
+                                                    img_array = np.fromfile(full_path, np.uint8)
+                                                    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+                                                    imgs_ = imgs_set_(500, 250, 800, 500, cla, img, 0.75)
+                                                    if imgs_ is not None and imgs_ != False:
+                                                        print("monster_info_close_btn", imgs_)
+                                                        click_pos_reg(imgs_.x, imgs_.y, cla)
+                                                    QTest.qWait(500)
 
 
 
