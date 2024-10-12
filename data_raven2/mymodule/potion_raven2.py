b0a519078e584423c0f9c9bd5c3e879446e54199
coob <koosanghwi@gmail.com>
2024-10-13 03:57:25 +0900

    ver 1.5302

--- data_raven2/mymodule/potion_raven2.py
+++ data_raven2/mymodule/potion_raven2.py
@@ -30,7 +30,7 @@ def potion_check(cla):
                 full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\juljun_potion\\" + str(i) + ".PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(450, 1000, 477, 1020, cla, img, 0.8)
+                imgs_ = imgs_set_(450, 1000, 477, 1020, cla, img, 0.85)
                 if imgs_ is not None and imgs_ != False:
                     many = i * 100
                     print_say = str(many) + "개 이상"
@@ -54,7 +54,7 @@ def potion_check(cla):
                     full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\juljun_potion\\" + str(i) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(450, 840, 477, 970, cla, img, 0.8)
+                    imgs_ = imgs_set_(450, 840, 477, 970, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         many = i * 100
                         print_say = str(many) + "개 이상"
@@ -71,7 +71,7 @@ def potion_check(cla):
                     full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\out_potion\\" + str(i) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(270, 982, 288, 1000, cla, img, 0.8)
+                    imgs_ = imgs_set_(270, 982, 288, 1000, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         many = i * 100
                         print_say = str(many) + "개 이상"
@@ -95,7 +95,7 @@ def potion_check(cla):
                         full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\out_potion\\" + str(i) + ".PNG"
                         img_array = np.fromfile(full_path, np.uint8)
                         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                        imgs_ = imgs_set_(270, 860, 288, 960, cla, img, 0.8)
+                        imgs_ = imgs_set_(270, 860, 288, 960, cla, img, 0.85)
                         if imgs_ is not None and imgs_ != False:
                             many = i * 100
                             print_say = str(many) + "개 이상"
