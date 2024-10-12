ee198fa2870cdccb1d6ad5a56c485d05714f6c27
coob <koosanghwi@gmail.com>
2024-10-13 04:35:43 +0900

    ver 1.5304

--- data_raven2/mymodule/potion_raven2.py
+++ data_raven2/mymodule/potion_raven2.py
@@ -45,7 +45,7 @@ def potion_check(cla):
                 full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\juljun_potion\\" + str(i) + ".PNG"
                 img_array = np.fromfile(full_path, np.uint8)
                 img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                imgs_ = imgs_set_(450, 1000, 477, 1020, cla, img, 0.8)
+                imgs_ = imgs_set_(450, 1000, 477, 1020, cla, img, 0.85)
                 if imgs_ is not None and imgs_ != False:
                     many = i * 100
                     print_say = str(many) + "개 이상"
@@ -73,7 +73,7 @@ def potion_check(cla):
                     full_path = "c:\\my_games\\raven2\\data_raven2\\imgs\\potion\\juljun_potion\\" + str(i) + ".PNG"
                     img_array = np.fromfile(full_path, np.uint8)
                     img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-                    imgs_ = imgs_set_(450, 840, 477, 970, cla, img, 0.8)
+                    imgs_ = imgs_set_(450, 840, 477, 970, cla, img, 0.85)
                     if imgs_ is not None and imgs_ != False:
                         many = i * 100
                         print_say = str(many) + "개 이상"
